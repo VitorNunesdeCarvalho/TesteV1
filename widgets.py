b166cc41b62b82cf9dfd3273f5ad2f1d549399e3
Vitor.Nunes <vitornunesdecarvalho@gmail.com>
2024-04-25 17:42:49 -0300

    teste1

---
+++ widgets.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.uix.label import Label
+class MinhaApp(App):
+    def build(self):
+        return Label(text= "Olá Senai!", font_size=200)
+if __name__=="__main__":
+    MinhaApp().run()
